795d3224865f4ca1b90fa5f53f1d86cee198c35a
kbouse <kouse@126.com>
2017-10-19 03:06:02 -0400

    update app.py

--- www/app.py
+++ www/app.py
@@ -6,17 +6,17 @@ import logging; logging.basicConfig(level=logging.INFO)
 import asyncio, os, json, time
 from datetime import datetime
 
-for aiohttp import web
+from aiohttp import web
 
 def index(request):
-    return web.Response(boby=b'<h1>Awesome</h1>')
+    return web.Response(body=b'<h1>Awesome</h1>',content_type='text/html')
 
 @asyncio.coroutine
 def init(loop):
     app = web.Application(loop=loop)
-    app.router.add_route('GET' ,'/' index)
-    srv = yield from loop.create+server(app.make_handler(),'170.0.0.1',9000)
-    logging.info('server started at http://127.0.0.1:9000....')
+    app.router.add_route('GET' ,'/', index)
+    srv = yield from loop.create_server(app.make_handler(),'45.78.13.172',9000)
+    logging.info('server started at http://45.78.131.172:9000....')
     return srv
 
 loop = asyncio.get_event_loop()
